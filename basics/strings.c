81b962ec23ea40dc9fddaa996b40ab106cbdc4af
Henrique Magalhães <henriqueheron1@gmail.com>
2025-03-12 06:56:23 -0300

    string comparisons

--- basics/strings.c
+++ basics/strings.c
@@ -2,12 +2,18 @@
 #include <string.h>
 
 int main() {
-    char title[] = "Lord of the Rings";
+    /* char title[] = "Lord of the Rings";
     int titleSize = strlen(title);
     printf("titleSize: %d\n", titleSize);
     for (int i = 0; i < titleSize; i++) {
         printf("%c", title[i]);
     }
+ */
+
+    char * nameOne = "Carlos";
+    char nameTwo[] = "Batista";
+
+    printf("Names match: %s\n", strcmp(nameOne, nameTwo) == 1 ? "false" : "true");
     
     return 0;
 }
