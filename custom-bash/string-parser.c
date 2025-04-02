6f67bca483ec3761c7188e833ce06193c06917ea
Henrique Magalhães <henriqueheron1@gmail.com>
2025-04-02 08:57:39 -0300

    add functionality to ls cmd

--- custom-bash/string-parser.c
+++ custom-bash/string-parser.c
@@ -1,10 +1,23 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <dirent.h>
 
 void parseCmd(char cmd[]) {
+	DIR* fd;
+	struct dirent* in_file;
+
 	if (strcmp(cmd, "ls") == 0) {
 		printf("Listing items\n");
+		fd = opendir(".");
+		if (fd == NULL) {
+			fprintf(stderr, "Error: failed to open directory");
+			exit(1);
+		}
+
+		while ((in_file = readdir(fd))) {
+			printf("%s\n", in_file->d_name);
+		}
 	}
 }
 
